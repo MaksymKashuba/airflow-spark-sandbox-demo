513f2b27d0ef493acf16c4c0b0e7c8ae5d2ae02d
MaksymKashuba <maksymkashuba2003@gmail.com>
2025-04-18 23:15:19 +0300

    Fixed java_job x2

--- dags/spark_airflow.py
+++ dags/spark_airflow.py
@@ -36,7 +36,7 @@ java_job = SparkSubmitOperator(
     task_id="java_job",
     conn_id="spark-conn",
     application="jobs/java/spark-job/target/spark-job-1.0-SNAPSHOT.jar",
-    java_class="com.example.sparkjob.WordCountJob",
+    java_class="com.example.sparkjob.wordcount",
     dag=dag
 )
 
