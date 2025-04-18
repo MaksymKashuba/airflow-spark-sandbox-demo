3eabecd8dab2c7f50d91a7e1b2e207340eba97e1
MaksymKashuba <maksymkashuba2003@gmail.com>
2025-04-18 19:37:36 +0300

    Fixed DAG operator exec (finally)

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,20 @@
-FROM apache/airflow:latest-python3.11
+FROM apache/airflow:2.9.0-python3.12
 
 USER root
-RUN apt-get update && \
-    apt-get install -y gcc python3-dev default-jdk && \
-    apt-get clean
 
-ENV JAVA_HOME=/usr/lib/jvm/java-11-openjdk-arm64
+RUN apt-get update -y && \
+    DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
+    gcc \
+    python3-dev \
+    default-jdk \
+    procps && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+ENV JAVA_HOME /usr/lib/jvm/default-java
 
 USER airflow
 
-RUN pip install apache-airflow apache-airflow-providers-apache-spark pyspark
+RUN pip install --no-cache-dir apache-airflow-providers-openlineage==1.12.1
+
+RUN pip install --no-cache-dir apache-airflow apache-airflow-providers-apache-spark pyspark
--- dags/spark_airflow.py
+++ dags/spark_airflow.py
@@ -3,34 +3,32 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 
-dag = DAG (
-    dag_id='spark_airflow_dag',
-    default_args={
-        'owner': 'Maksym Kashuba',
-        'start_date': airflow.utils.dates.days_ago(1),
-        'retries': 1,
-        'retry_delay': 300,
+dag = DAG(
+    dag_id = "sparking_flow",
+    default_args = {
+        "owner": "Maksym Kashuba",
+        "start_date": airflow.utils.dates.days_ago(1)
     },
-    schedule_interval='@daily',
+    schedule_interval = "@daily"
 )
 
 start = PythonOperator(
-    task_id='start',
-    python_callable=lambda: print("Starting the DAG"),
-    dag=dag,
+    task_id="start",
+    python_callable = lambda: print("Jobs started"),
+    dag=dag
 )
 
 python_job = SparkSubmitOperator(
-    task_id='python_job',
-    conn_id='spark_default',
-    application='/path/to/your/wordcountjob.py',
+    task_id="python_job",
+    conn_id="spark-conn",
+    application="jobs/python/wordcountjob.py",
     dag=dag
 )
 
 end = PythonOperator(
-    task_id='end',
-    python_callable=lambda: print("Completed the DAG"),
-    dag=dag,
+    task_id="end",
+    python_callable = lambda: print("Jobs completed successfully"),
+    dag=dag
 )
 
 start >> python_job >> end
