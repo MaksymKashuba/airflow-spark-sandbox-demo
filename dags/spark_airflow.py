97eabace1c995f68175cedb8a9c41041cc9f361c
MaksymKashuba <maksymkashuba2003@gmail.com>
2025-04-18 17:20:05 +0300

    Added DAG

---
+++ dags/spark_airflow.py
@@ -0,0 +1,28 @@
+import airflow
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.spark.operators.spark_submit import SparkSubmitOperator
+
+dag = DAG (
+    dag_id='spark_airflow_dag',
+    default_args={
+        'owner': 'Maksym Kashuba',
+        'start_date': airflow.utils.dates.days_ago(1),
+        'retries': 1,
+        'retry_delay': 300,
+    },
+    schedule_interval='@daily',
+)
+
+start = PythonOperator(
+    task_id='start',
+    python_callable=lambda: print("Starting the DAG"),
+    dag=dag,
+)
+
+python_job = SparkSubmitOperator(
+    task_id='python_job',
+    conn_id='spark_default',
+    application='/path/to/your/wordcountjob.py',
+    dag=dag
+)
