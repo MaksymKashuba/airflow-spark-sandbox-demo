22fa745ca945abf965b841be9b881f650eabf2ce
MaksymKashuba <maksymkashuba2003@gmail.com>
2025-04-18 23:19:59 +0300

    Fixed the java_job x3

--- dags/spark_airflow.py
+++ dags/spark_airflow.py
@@ -36,7 +36,7 @@ java_job = SparkSubmitOperator(
     task_id="java_job",
     conn_id="spark-conn",
     application="jobs/java/spark-job/target/spark-job-1.0-SNAPSHOT.jar",
-    java_class="com.example.sparkjob.wordcount",
+    java_class="com.airscholar.spark.wordcount",
     dag=dag
 )
 
